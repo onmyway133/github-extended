c79188ec80862cb6d4026dc3a86c7e011adb0197
Khoa Pham <onmyway133@gmail.com>
2016-05-22 09:03:21 +0200

    Show more repositories

--- app/scripts.babel/content.js
+++ app/scripts.babel/content.js
@@ -17,18 +17,48 @@ function fetchRepositories(username) {
   .then((response) => {
     return response.json()
   }).then((json) => {
-    process(username, json)
+    process(json)
   })
 }
 
-function process(username, repositories) {
+function process(repositories) {
   const processedRepositories =
   repositories
   .filter((repo) => {
     return repo.private == false && repo.fork == false
   }).sort((repo1, repo2) => {
-    return repo1.stargazers_count < repo2.stargazers_count
+    return (repo1.stargazers_count < repo2.stargazers_count) ? 1 : -1
   })
 
-  console.log(processedRepositories)
+  add(processedRepositories)
+}
+
+function add(repositories) {
+  const ul = popularRepositoriesElement()
+  const sample_li = $(ul).find('li:first-child')
+
+  repositories.forEach((repo) => {
+    const li = sample_li.clone()
+
+    // URL
+    $(li).find('a').attr('href', repo.html_url)
+
+    // Name
+    const name = $(li).find('.repo-and-owner.css-truncate-target .repo')
+    $(name).attr('title', repo.name)
+    $(name).text(repo.name)
+
+    // Star
+    $(li).find('.stars').text(repo.stargazers_count)
+
+    // Description
+    $(li).find('.repo-description.css-truncate-target').text(repo.description)
+
+    $(ul).append(li)
+  })
+}
+
+function popularRepositoriesElement() {
+  const element = $('.contributions-tab .columns.popular-repos .column.one-half:first-child .boxed-group.flush ul')
+  return element
 }
--- app/scripts/content.js
+++ app/scripts/content.js
@@ -16,16 +16,46 @@ function fetchRepositories(username) {
   fetch('https://api.github.com/users/' + username + '/repos?visibility=public&affiliation=owner&per_page=100').then(function (response) {
     return response.json();
   }).then(function (json) {
-    process(username, json);
+    process(json);
   });
 }
 
-function process(username, repositories) {
+function process(repositories) {
   var processedRepositories = repositories.filter(function (repo) {
     return repo.private == false && repo.fork == false;
   }).sort(function (repo1, repo2) {
-    return repo1.stargazers_count < repo2.stargazers_count;
+    return repo1.stargazers_count < repo2.stargazers_count ? 1 : -1;
   });
 
-  console.log(processedRepositories);
+  add(processedRepositories);
+}
+
+function add(repositories) {
+  var ul = popularRepositoriesElement();
+  var sample_li = $(ul).find('li:first-child');
+
+  repositories.forEach(function (repo) {
+    var li = sample_li.clone();
+
+    // URL
+    $(li).find('a').attr('href', repo.html_url);
+
+    // Name
+    var name = $(li).find('.repo-and-owner.css-truncate-target .repo');
+    $(name).attr('title', repo.name);
+    $(name).text(repo.name);
+
+    // Star
+    $(li).find('.stars').text(repo.stargazers_count);
+
+    // Description
+    $(li).find('.repo-description.css-truncate-target').text(repo.description);
+
+    $(ul).append(li);
+  });
+}
+
+function popularRepositoriesElement() {
+  var element = $('.contributions-tab .columns.popular-repos .column.one-half:first-child .boxed-group.flush ul');
+  return element;
 }
